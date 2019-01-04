e327600507d3c7fc315d1f1cfc761b7edf9023b2
BambangPriam <45525619+BambangPriam@users.noreply.github.com>
2019-01-04 20:57:06 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int E, m, c;
+    cout << "PROGRAM MENGHITUNG RUMUS E=MC2 \n"<< endl;
+    cout << "Masukkan nilai m : "; cin>>m;
+    cout << "Masukkan nilai c : "; cin >>c;
+
+    E=m*c*c;
+    cout << "E: " << E<<endl;
+    cout << "Nilai E adalah : "<< E;
+    return 0;
+    }
